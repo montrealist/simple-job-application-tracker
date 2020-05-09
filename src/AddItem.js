1ee4a72056a2098d1499f0e4ad869c261d2577cf
Max <max@Maxs-MacBook-Air.local>
2020-05-09 15:18:41 -0400

    added state logic

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,32 +1,58 @@
 import React, { useState } from 'react';
 
 export default function AddItem(props) {
-    const [state, setState] = useState({});
+    const [company, setCompany] = useState('');
+    const [position, setPosition] = useState('');
+    const [notes, setNotes] = useState('');
+    const [error, setError] = useState('');
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log('submit');
+        if (company === '' || position === '') {
+            setError('Company and position are mandatory fields');
+            return;
+        }
 
+        console.log('submit');
+        props.onSave({ company, position, notes });
     };
 
+    const validate = (event) => {
+        if (event) {
+            setError('');
+        }
+    }
+    const reset = () => {
+        setCompany('');
+        setPosition('');
+        setNotes('');
+    }
     const onClear = (e) => {
         e.preventDefault();
-        console.log('clear');
+        reset();
     };
 
     return (<div className="f3 list pl0 mt0 measure-wide-ns center">
         <form className="pa4 black-80">
             <div className="measure">
                 <label htmlFor="company" className="f6 b db mb2">Company</label>
-                <input id="company" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
+                <input id="company" value={company} onChange={e => {
+                    validate(e.target.value)
+                    setCompany(e.target.value)
+                }} className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
             </div>
             <div className="measure">
                 <label htmlFor="position" className="f6 b db mb2">Position</label>
-                <input id="position" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
+                <input id="position" value={position} onChange={e => {
+                    validate(e.target.value)
+                    setPosition(e.target.value)
+                }} className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
             </div>
             <div className="measure">
                 <label htmlFor="notes" className="f6 b db mb2">Notes <span className="normal black-60">(optional)</span></label>
-                <textarea id="notes" name="notes" className="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" aria-describedby="comment-desc"></textarea>
+                <textarea id="notes" value={notes} onChange={e => {
+                    setNotes(e.target.value)
+                }} name="notes" className="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" aria-describedby="comment-desc"></textarea>
                 <small id="name-desc" className="f6 black-60 db mb2">Any relevant notes about the company or the process.</small>
             </div>
             <div className="measure tr">
